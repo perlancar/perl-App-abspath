f9d4c0d1122db6005893005310747518d1d5d57e
perlancar (on pc-home) <perlancar@gmail.com>
2021-07-15 07:07:07 +0700

    Initial commit

---
+++ lib/App/abspath.pm
@@ -0,0 +1,16 @@
+package App::abspath;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+#ABSTRACT: Print the absolute path
+
+=head1 DESCRIPTION
+
+See the included script L<abspath>.
+
+
+=head1 SEE ALSO
